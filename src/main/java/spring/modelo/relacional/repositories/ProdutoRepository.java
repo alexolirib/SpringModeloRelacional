fb9a9456c3de680699fa2e94c0ebfee733e40a95
alexandre ribeiro <alexolirib@gmail.com>
2018-06-21 18:10:56 -0300

    up

--- src/main/java/spring/modelo/relacional/repositories/ProdutoRepository.java
+++ src/main/java/spring/modelo/relacional/repositories/ProdutoRepository.java
@@ -1,13 +1,25 @@
 package spring.modelo.relacional.repositories;
 
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import spring.modelo.relacional.domain.Categoria;
 import spring.modelo.relacional.domain.Produto;
 
 //camada acesso a dados 
 @Repository
 //JpaRepository<obj do tipo categoria, tipo do atributo identificador>--tipo especial do spring para acessar os dados
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+	
+	//esse método não tem a implementação desse método no spring preciso botar @Query(aqui fica o JPQL)
+	@Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj INNER JOIN obj.categorias cat WHERE obj.nome LIKE %:nome% AND cat IN :categorias")
+	Page<Produto> search(@Param("nome" /* nome dos paramétro no JPQL*/) String nome,@Param("categorias") List<Categoria> categorias, Pageable linePerPage);
 
 }
--- src/main/java/spring/modelo/relacional/services/ProdutoService.java
+++ src/main/java/spring/modelo/relacional/services/ProdutoService.java
@@ -1,5 +1,41 @@
 package spring.modelo.relacional.services;
 
-public class ProdutoService {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+
+import spring.modelo.relacional.domain.Categoria;
+import spring.modelo.relacional.domain.Produto;
+import spring.modelo.relacional.repositories.CategoriaRepository;
+import spring.modelo.relacional.repositories.ProdutoRepository;
+import spring.modelo.relacional.services.Exception.ObjectNotFoundException;
+
+@Service
+public class ProdutoService {
+	
+	@Autowired
+	private ProdutoRepository repo;
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	public Produto findById(Integer id) {
+		Optional<Produto> obj = repo.findById(id);
+		return obj.orElseThrow(()-> new ObjectNotFoundException(
+				"Objeto não encontrado "+ id + ", tipo " + Produto.class.getName()));
+	}
+	
+	//com um id produto irá vir lista de id de categoria	
+	public Page<Produto> search(String nome, List<Integer> ids, Integer page, Integer linePerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linePerPage, Direction.valueOf(direction), orderBy);
+		
+		//buscando as categorias com a lista de id daquele produto
+		List<Categoria> categorias =  categoriaRepository.findAllById(ids);
+		return repo.search(nome, categorias, pageRequest);
+	}
 }
